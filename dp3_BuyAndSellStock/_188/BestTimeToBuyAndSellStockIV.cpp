fa10a222eb3e8f9bd7bd747eef0742e3c650ddc4
FUZHENGXIN <fuzhengxin@kingsoft.com>
2022-11-13 23:58:05 +0800

    committed by fsindustry@2022/11/13 about dynamic programming;

--- dp3_BuyAndSellStock/_188/BestTimeToBuyAndSellStockIV.cpp
+++ dp3_BuyAndSellStock/_188/BestTimeToBuyAndSellStockIV.cpp
@@ -8,9 +8,6 @@
 using namespace std;
 
 class Solution {
-private:
-    vector<vector<vector<int>>> dp;
-
 public:
     int maxProfit(int K, vector<int> &prices) {
 
@@ -19,30 +16,33 @@ public:
             return 0;
         }
 
-        dp = vector<vector<vector<int>>>(n, vector<vector<int>>(K + 1, vector<int>(2, 0)));
+        int cache[n][K + 1][2];
+        memset(&cache, 0, n * (K + 1) * 2);
 
-        // 第0次交易，不允许交易，故利润为0，也不可能持有股票；
+        // k == 0
         for (int i = 0; i < n; i++) {
-            dp[i][0][0] = 0;
-            dp[i][0][1] = -prices[0];
+            cache[i][0][0] = 0;
+            // 无意义，故给个负数
+            cache[i][0][1] = -prices[0];
         }
 
-        // 第0天，无交易，则不持有股票利润为0，也不可能持有股票；
+        // i == 0
         for (int k = 0; k <= K; k++) {
-            dp[0][k][0] = 0;
-            dp[0][k][1] = -prices[0];
+            cache[0][k][0] = 0;
+            // 持有股票，则利润为负数
+            cache[0][k][1] = -prices[0];
         }
 
-        for (int i = 1; i < n; i++) { // 从第1天开始交易
-            for (int k = K; k >= 1; k--) { // 一开始无交易，故可以交易K次
+        for (int i = 1; i < n; i++) { // 从第i天开始交易
+            for (int k = 1; k <= K; k++) { // 交易了k次
                 // 今天未持有 = max（昨天未持有&今天继续不持有的利润，昨天持有&今天卖出的利润）
-                dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                cache[i][k][0] = max(cache[i - 1][k][0], cache[i - 1][k][1] + prices[i]);
                 // 今天持有的最大利润 = max（昨天持有&今天继续持有的利润，昨天未持有&今天买入的利润）
-                dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+                cache[i][k][1] = max(cache[i - 1][k][1], cache[i - 1][k - 1][0] - prices[i]);
             }
         }
 
-        return dp[n - 1][K][0];
+        return cache[n - 1][K][0];
     }
 };
 
